7bf9227ffb35454d7cdf25530b03323de7ff7dbd
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2021-07-17 12:00:56 -0600

    configuración de babel para el servidor, alias disponibles

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,25 @@
+const path = require('path')
 require('ignore-styles')
 require('@babel/polyfill')
 require('@babel/register')({
     presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                alias: {
+                    components: path.resolve(
+                        __dirname,
+                        '../frontend/components/'
+                    ),
+                    assets: path.resolve(__dirname, '../frontend/assets/'),
+                    utils: path.resolve(__dirname, '../frontend/utils/'),
+                    actions: path.resolve(__dirname, '../frontend/actions/'),
+                    reducers: path.resolve(__dirname, '../frontend/reducers/'),
+                },
+            },
+        ],
+    ],
 })
 
 require('asset-require-hook')({
